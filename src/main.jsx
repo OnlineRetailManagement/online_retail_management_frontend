c9dd748ceb7dd8ab5266cecd0f2f6eccd3ee6308
Deep Vadukiya <dipvadukiya06@gmail.com>
2025-05-15 23:03:14 +0200

    ORMF01 | removed boiler plate code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 //
 
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -8,7 +8,7 @@ import App from "./App.jsx";
 // ----------------------------------------
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.Fragment>
     <App />
-  </StrictMode>
+  </React.Fragment>
 );
