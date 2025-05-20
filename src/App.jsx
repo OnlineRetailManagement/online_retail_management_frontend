7ccaa535ab1789d52a101770ccbefd48ad50dcd8
Deep Vadukiya <dipvadukiya06@gmail.com>
2025-05-20 23:01:46 +0200

    ORM07 | added taiilwind, integrated shadcn for ui components and added basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 //
 
 import { Fragment } from "react";
+import { Button } from "@/components/ui/button";
 
 // ----------------------------------------
 
 function App() {
   return (
     <Fragment>
-      <p>Hello World ...!!!!</p>
+      <div className="p-4">
+        <p>Hello World ...!!!!</p>
+        <Button className="mt-4">Click me</Button>
+      </div>
     </Fragment>
   );
 }
