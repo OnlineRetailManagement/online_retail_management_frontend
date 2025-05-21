ed73b73ee399a5d22e45d0d76e74570fc3d3f703
Deep Vadukiya <dipvadukiya06@gmail.com>
2025-05-21 21:24:32 +0200

    ORM11 | added register page, added ui and components

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -11,7 +11,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "../../components/ui/card";
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 // routes
