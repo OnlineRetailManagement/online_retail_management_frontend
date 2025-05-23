a20d84ada80b108f37e5e2985dea9efe0c15cf46
Deep Vadukiya <dipvadukiya06@gmail.com>
2025-05-23 14:47:30 +0200

    ORM13 | updaetd guest guard and redirected to default pages based on user roles

--- src/guards/GuestGuard.jsx
+++ src/guards/GuestGuard.jsx
@@ -4,15 +4,22 @@ import { Navigate } from "react-router-dom";
 // hooks
 import useAuth from "../hooks/useAuth";
 // routes
-import { USER_PATHS } from "../routes/paths";
+import { ADMIN_PATHS, USER_PATHS, VENDOR_PATHS } from "../routes/paths";
 
 // ----------------------------------------------------------------------
 
 export default function GuestGuard({ children }) {
   const { isAuthenticated } = useAuth();
 
+  const userData = JSON.parse(window.localStorage.getItem("userData"));
+  const userRole = userData?.role ?? null;
+
   if (isAuthenticated) {
-    return <Navigate to={USER_PATHS.dashboard} />;
+    if (userRole === "user") return <Navigate to={USER_PATHS.dashboard} />;
+    else if (userRole === "vendor")
+      return <Navigate to={VENDOR_PATHS.dashboard} />;
+    else if (userRole === "admin")
+      return <Navigate to={ADMIN_PATHS.dashboard} />;
   }
 
   return <>{children}</>;
