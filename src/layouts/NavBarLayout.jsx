415891501b346e0b08654b96e02a4e89e45fba4b
Deep Vadukiya <dipvadukiya06@gmail.com>
2025-06-04 23:06:52 +0200

    ORM16 | fix: show user name at the nav footer | integrated ui with the api resopnse

--- src/layouts/NavBarLayout.jsx
+++ src/layouts/NavBarLayout.jsx
@@ -236,7 +236,9 @@ const NavUser = (props) => {
             {user?.name ?? "NU"}
           </Avatar>
           <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-semibold">{user?.name ?? "-"}</span>
+            <span className="truncate font-semibold">
+              {user?.user?.firstName + " " + user?.user?.lastName}
+            </span>
             <span className="truncate text-xs">{user?.email ?? "-"}</span>
           </div>
         </SidebarMenuButton>
