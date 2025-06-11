b1e39ae1f9a544657717082141f9efc7f9400a22
Deep Vadukiya <dipvadukiya06@gmail.com>
2025-06-11 22:53:04 +0200

    ORM20 | feat: attachment related updates

--- src/config.js
+++ src/config.js
@@ -3,3 +3,6 @@
 // base API end-point
 export const BASE_URL =
   import.meta.env.REACT_APP_HOST_API_KEY || "http://localhost:8080";
+
+export const BASE_URL_IMG =
+  "http://localhost:8080/public/attachment?file_name=";
