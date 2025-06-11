c6605379163ebfd87bf88ec7982156b0fbd14e51
Deep Vadukiya <dipvadukiya06@gmail.com>
2025-06-11 19:49:42 +0200

    ORM20 | feat: vendors: orders: added tab to get the active and past orders

--- src/pages/Vendors/Orders/index.jsx
+++ src/pages/Vendors/Orders/index.jsx
@@ -11,6 +11,7 @@ import {
   SelectItem,
 } from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 // lucid-icon
 import { MapPin, Banknote } from "lucide-react";
 // redux
@@ -60,7 +61,7 @@ export default function Orders() {
 
   useEffect(() => {
     dispatch(getOrder(user?.user?.id, isGetActiveOrders, userRole));
-  }, [dispatch]);
+  }, [dispatch, isGetActiveOrders]);
 
   useEffect(() => {
     if (isOrderUpdated) {
@@ -72,76 +73,89 @@ export default function Orders() {
     dispatch(updateOrder({ order_id: orderData?.id, order_status: e }));
   };
 
+  const handleTabChange = (newVal) => {
+    setIsGetActiveOrders(newVal === "pastOrders" ? false : true);
+  };
+
   return (
     <div className="ml-4 h-screen">
       <div className="flex my-5">
         <p className="font-semibold text-gray-700">Vendors: Orders</p>
       </div>
 
-      <div className="flex flex-col gap-3 pb-6">
-        {orders?.map((ordr) => {
-          return (
-            <Card key={`vendor-order-${ordr.id}`}>
-              <CardContent>
-                <div className="flex">
-                  <div className="basis-8/10">
-                    <p>Product: {ordr?.product?.title}</p>
-                    <p>Description: {ordr?.product?.description}</p>
-                    <p>Delivery time: {ordr?.product?.delivery_time}</p>
-                    <p>Quantity: {ordr?.quantity}</p>
+      <Tabs defaultValue="activeOrders" onValueChange={handleTabChange}>
+        <TabsList>
+          <TabsTrigger value="activeOrders">Active Orders</TabsTrigger>
+          <TabsTrigger value="pastOrders">Past Orders</TabsTrigger>
+        </TabsList>
 
-                    <div className="flex gap-2 items-center pt-1">
-                      <MapPin size={16} />
-                      <p>
-                        Delivery Address:{" "}
-                        {ordr?.address?.address_line1 +
-                          " " +
-                          ordr?.address?.address_line2 +
-                          ", " +
-                          ordr?.address?.zip_code +
-                          " " +
-                          ordr?.address?.city +
-                          ". " +
-                          ordr?.address?.country}
-                      </p>
+        <div className="flex flex-col gap-3 py-3">
+          {orders?.map((ordr) => {
+            return (
+              <Card key={`vendor-order-${ordr.id}`}>
+                <CardContent>
+                  <div className="flex">
+                    <div className="basis-8/10">
+                      <p>Product: {ordr?.product?.title}</p>
+                      <p>Description: {ordr?.product?.description}</p>
+                      <p>Delivery time: {ordr?.product?.delivery_time}</p>
+                      <p>Quantity: {ordr?.quantity}</p>
+
+                      <div className="flex gap-2 items-center pt-1">
+                        <MapPin size={16} />
+                        <p>
+                          Delivery Address:{" "}
+                          {ordr?.address?.address_line1 +
+                            " " +
+                            ordr?.address?.address_line2 +
+                            ", " +
+                            ordr?.address?.zip_code +
+                            " " +
+                            ordr?.address?.city +
+                            ". " +
+                            ordr?.address?.country}
+                        </p>
+                      </div>
+
+                      <div className="flex gap-2 items-center pt-1">
+                        <Banknote size={16} />
+                        <p>Payment Status: Done</p>
+                      </div>
                     </div>
 
-                    <div className="flex gap-2 items-center pt-1">
-                      <Banknote size={16} />
-                      <p>Payment Status: Done</p>
+                    <div className="basis-2/10">
+                      <div className="grid gap-2">
+                        <Label htmlFor="order_status">Order Status</Label>
+                        <Select
+                          name="order_status"
+                          id="order_status"
+                          onValueChange={(e) =>
+                            handleOrderStatusChange(e, ordr)
+                          }
+                        >
+                          <SelectTrigger className="w-full" id="rows-per-page">
+                            <SelectValue
+                              placeholder={ordr?.order_status ?? "Processing"}
+                            />
+                          </SelectTrigger>
+
+                          <SelectContent side="top">
+                            {orderStatus.map((stat) => (
+                              <SelectItem key={stat.id} value={stat.name}>
+                                {stat.name}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                      </div>
                     </div>
                   </div>
-
-                  <div className="basis-2/10">
-                    <div className="grid gap-2">
-                      <Label htmlFor="order_status">Order Status</Label>
-                      <Select
-                        name="order_status"
-                        id="order_status"
-                        onValueChange={(e) => handleOrderStatusChange(e, ordr)}
-                      >
-                        <SelectTrigger className="w-full" id="rows-per-page">
-                          <SelectValue
-                            placeholder={ordr?.order_status ?? "Processing"}
-                          />
-                        </SelectTrigger>
-
-                        <SelectContent side="top">
-                          {orderStatus.map((stat) => (
-                            <SelectItem key={stat.id} value={stat.name}>
-                              {stat.name}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                    </div>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          );
-        })}
-      </div>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+      </Tabs>
     </div>
   );
 }
